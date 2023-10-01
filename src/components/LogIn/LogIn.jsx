d6a6e1ce6439f3b7c673ed0d0cc6802701ff93d2
emran <shuvoorba28@gmail.com>
2023-10-01 16:27:58 +0600

    I'm done

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,9 +1,49 @@
-
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import app from "../../firebase/firebase.config";
+import { useState } from "react";
 
 const LogIn = () => {
+
+    const [user, setUser] = useState([])
+
+    const auth = getAuth(app);
+
+    const provider = new GoogleAuthProvider()
+
+
+
+    const handleLogIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser)
+            })
+            .error(error => {
+                console.log(error)
+            })
+        console.log('button clicked')
+    }
+
+    const handleSignOut = () => {
+        const auth = getAuth(app)
+        signOut(auth)
+            .then(() => {
+                setUser(null)
+            })
+            .error(() => {
+                console.log(error)
+            })
+        console.log('sign out button clicked')
+    }
+
     return (
         <div>
-            this is login page            
+            <button onClick={handleLogIn}>Login</button>
+            <button onClick={handleSignOut}>Sign out</button>
+            <h2>user: {user?.displayName}</h2>
+            <p>email: {user?.email}</p>
+            <img src={user?.photoURL} alt="" />
         </div>
     );
 };
